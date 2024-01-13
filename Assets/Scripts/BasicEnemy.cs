66d5788a9fd6f9d4b79c4c8b5b4277186b1f00c2
Leonardo Pariano <126412174+LeoPepperoni@users.noreply.github.com>
2024-01-13 13:36:40 -0500

    pushing a few enemies

---
+++ Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    public float speed = 5f;
+    private int direction = 1;
+    public GameObject player;
+
+    public GameObject projectilePrefab;
+    public float shootInterval = 1.0f;
+    private float shootTimer;
+
+    private void Start()
+    {
+        shootTimer = shootInterval;
+        player = GameObject.Find("Sun"); // Find the game object named "Sun" and set it as the player
+    }
+
+    private void Update()
+    {
+        transform.position += new Vector3(speed * direction * Time.deltaTime, 0, 0);
+
+        Vector2 screenPosition = Camera.main.WorldToViewportPoint(transform.position);
+        if (screenPosition.x > 1 || screenPosition.x < 0)
+        {
+            direction *= -1;
+        }
+
+        Vector2 directionToPlayer = player.transform.position - transform.position;
+        transform.up = directionToPlayer;
+
+        shootTimer -= Time.deltaTime;
+        if (shootTimer <= 0)
+        {
+            ShootProjectile();
+            shootTimer = shootInterval;
+        }
+    }
+
+    void ShootProjectile()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/HeatSeeker.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatSeeker : MonoBehaviour
+{
+    public GameObject player;
+    public float speed = 5f;
+    public float selfDestructTime = 5f;
+
+    private float timer;
+
+    private void Start()
+    {
+        timer = selfDestructTime;
+    }
+
+    private void Update()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+
+        // Rotate towards the player
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+
+        // Move towards the player
+        transform.position += direction * speed * Time.deltaTime;
+
+        // Self-destruct timer
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

78692d0f60b34812fa3e15369b182e08006fd88a
Leonardo Pariano <126412174+LeoPepperoni@users.noreply.github.com>
2024-01-13 15:45:07 -0500

    pushing

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,25 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public float speed = 4.5f;
+    private Vector3 mousePos;
+    private Camera mainCam;
+    private Rigidbody2D rb;
+    public float force;
+
     void Start()
     {
-
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized*force;
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
     }
-
-    void Update()
+    void OnCollisionEnter2D(Collision2D col)
     {
-        transform.position += transform.up * Time.deltaTime * speed;
+        Debug.Log("OnCollisionEnter2D");
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag != "PlayerProjectileIgnore")
-        Destroy(gameObject);
-    }
     void OnBecameInvisible()
     {
         Destroy(gameObject);
+    }
+    void Update()
+    {
+        
     }
 }
